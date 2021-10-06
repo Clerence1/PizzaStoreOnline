90be03a6a70115ce27e309504b74ce581802db75
Clerence1 <mahlomola.lebogo@gmail.com>
2021-10-06 17:19:26 +0200

    Add project files.

---
+++ testing/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using PizzaStoreOnline;
+using PizzaStoreOnline.Controllers;
+
+namespace testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Index() { 
+
+            HomeController controller = new HomeController();
+
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
